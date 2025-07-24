54dff6aea8d0b57fc135f7cb680d0b20aebea606
WickeyR <149520847+WickeyR@users.noreply.github.com>
2025-07-23 19:55:40 -0500

    Added backwards movement

--- Island Survivor/Assets/Scripts/PlayerMovement.cs
+++ Island Survivor/Assets/Scripts/PlayerMovement.cs
@@ -1,49 +1,49 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody),typeof(Animator))]
-public class PlayerMovement : MonoBehaviour {
-    [Header("Movement")] [SerializeField] float moveSpeed = 10f; 
-    [SerializeField] float jumpForce = 6f;
-    [Header("Ground Check")] [SerializeField] float groundCheckDistance = 1.05f;
-    [SerializeField] LayerMask groundMask = ~0;
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")] [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float jumpForce = 6f;
+    [Header("Ground Check")] [SerializeField] private float groundCheckDistance = 1.05f;
+    [SerializeField] private LayerMask groundMask = ~0;
+    private Rigidbody _rb;
+    private Animator _anim;
+    private bool _jumpRequested;
 
-    Rigidbody _rb;
-    Animator _anim;
-    bool _jumpRequested;
-
-    void Awake() {
-        _rb   = GetComponent<Rigidbody>();
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
         _anim = GetComponent<Animator>();
     }
 
-    void Update() {
+    private void Update()
+    {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
-
-        Vector3 dir = (transform.right*x + transform.forward*z).normalized;
-        _rb.velocity = new Vector3(dir.x*moveSpeed, _rb.velocity.y, dir.z*moveSpeed);
-
-        // feed Speed param for Idle<->Run
-        _anim.SetFloat("Speed", new Vector2(x,z).magnitude);
-
-        if (Input.GetButtonDown("Jump"))
+        Vector3 dir = (transform.right * x + transform.forward * z).normalized;
+        _rb.velocity = new Vector3(dir.x * moveSpeed, _rb.velocity.y, dir.z * moveSpeed);
+        _anim.SetFloat("MoveZ", z);
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
             _jumpRequested = true;
             _anim.SetTrigger("Jump");
+        }
     }
 
-    void FixedUpdate() {
-        bool grounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundMask);
-
-        // feed ground & vertical velocity for your Jump transitions
-        _anim.SetBool("isGrounded", grounded);
-        _anim.SetFloat("yVelocity", _rb.velocity.y);
-
-        if (_jumpRequested && grounded) {
-            _rb.velocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
+    private void FixedUpdate()
+    {
+        if (_jumpRequested)
+        {
+            _rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
             _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            _anim.SetTrigger("Jump");
         }
-
         _jumpRequested = false;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundMask);
     }
 }
