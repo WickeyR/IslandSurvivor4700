a820b0983520d0ca06c54926788739adbe2d6e2d
Elsa Zheng <zhengelsa150@gmail.com>
2025-07-29 16:18:09 -0700

    Animal Update
    - changed to a goat, chicken animation was not working correctly
    - AI Movement for wandering
    - added more goats spread around the island

--- Island Survivor/Assets/Scripts/AIMovement.cs
+++ Island Survivor/Assets/Scripts/AIMovement.cs
@@ -18,30 +18,30 @@ public class AIMovement : MonoBehaviour
 
     int WalkDirection;
 
-    public bool isWalking;
+    public bool checkIsWalking;
 
     // Start is called before the first frame update
     void Start(){
         animator = GetComponent<Animator>();
 
-        //So that all the prefabs don't move/stop at the same time
+        //give the animal random walk and wait durations so that they're not all do the same thing
         walkTime = Random.Range(3, 6);
         waitTime = Random.Range(5, 7);
-
-
         waitCounter = waitTime;
         walkCounter = walkTime;
 
-        ChooseDirection();
+        ChooseDirection(); //chooses starting direction
     }//end of Start
 
     // Update is called once per frame
     void Update(){
-        if (isWalking){
-            animator.SetBool("isRunning", true);
+        if (checkIsWalking)
+        {
+            animator.SetBool("isWalking", true);
 
             walkCounter -= Time.deltaTime;
 
+            //resets the rotation of the creature's positioning based on WalkDirection
             switch (WalkDirection){
                 case 0:
                     transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
@@ -61,16 +61,19 @@ public class AIMovement : MonoBehaviour
                     break;
             }//end of switch
 
+            //will continue to walk until the walkCounter reaches 0
             if (walkCounter <= 0){
+                //switches position back to default idle position
                 stopPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-                isWalking = false;
+                checkIsWalking = false;
                 //stop movement
                 transform.position = stopPosition;
-                animator.SetBool("isRunning", false);
+                animator.SetBool("isWalking", false);
                 //reset the waitCounter
                 waitCounter = waitTime;
             }//end of if
         }//end of if
+        //will not walk until the waitCounter reaches 0
         else{
             waitCounter -= Time.deltaTime;
 
@@ -82,9 +85,13 @@ public class AIMovement : MonoBehaviour
 
 
     public void ChooseDirection(){
+        /*
+         * picks a random number from 0-3
+         * possible directions to face: forward, backwards, left, and right
+         */
         WalkDirection = Random.Range(0, 4);
 
-        isWalking = true;
+        checkIsWalking = true;
         walkCounter = walkTime;
     }//end of ChooseDirection
 }//end of AIMovement
---
+++ Island Survivor/Assets/UrsaAnimation/LOW POLY CUBIC - Goat and Sheep Pack/scripts/AnimationController.cs
@@ -0,0 +1,71 @@
+    using UnityEngine;
+    public class AnimationController : MonoBehaviour
+    {
+        /*
+        public Animator animator;
+        public string walkForwardAnimation = "walk_forward";
+        public string walkBackwardAnimation = "walk_backwards";
+        public string runForwardAnimation = "run_forward";
+        public string turn90LAnimation = "turn_90_L";
+        public string turn90RAnimation = "turn_90_R";
+        public string trotAnimation = "trot_forward";
+        public string sittostandAnimation = "sit_to_stand";
+        public string standtositAnimation = "stand_to_sit";
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                animator.Play(walkForwardAnimation);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                animator.Play(walkBackwardAnimation);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                animator.Play(runForwardAnimation);
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                animator.Play(turn90LAnimation);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                animator.Play(turn90RAnimation);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                animator.Play(trotAnimation);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                animator.Play(sittostandAnimation);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                animator.Play(standtositAnimation);
+            }
+        }
+        */
+        //modified from rabbits code from lecture
+        private Animator m_animator;
+        private AIMovement m_aiMovement;
+
+        void Start(){
+            m_animator = GetComponent<Animator>();
+            m_aiMovement = GetComponent<AIMovement>();
+        }//end of Start
+
+        void Update(){
+            if (m_animator != null && m_aiMovement != null){
+            // Update animator based on AI movement
+            m_animator.SetBool("isWalking", m_aiMovement.checkIsWalking);
+        }//end of if
+    }//end of Update
+}//end of AnimationController
