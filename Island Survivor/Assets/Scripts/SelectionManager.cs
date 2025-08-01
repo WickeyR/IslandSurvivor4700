a3a3baca7099cddad2287355a85622d4c5ef5d1e
Elsa Zheng <zhengelsa150@gmail.com>
2025-07-31 19:39:21 -0700

    Module 6 Requirements
    - adjusted animation and raycast issues
    - added weapon
    - tree chopping for wood
    - will drop fruit and wood
    - applied to one tree, will apply to other trees in next update

--- Island Survivor/Assets/Scripts/SelectionManager.cs
+++ Island Survivor/Assets/Scripts/SelectionManager.cs
@@ -8,6 +8,7 @@ public class SelectionManager : MonoBehaviour
 {
     public GameObject InteractionInfoUI; //allows us to turn UI element on and off
     Text interaction_text; //changing the text value directly in the Inspector
+    public float choppingDistance = 3f;
 
     private void Start(){
         interaction_text = InteractionInfoUI.GetComponent<Text>(); //assigned to internal text component
@@ -16,6 +17,7 @@ public class SelectionManager : MonoBehaviour
     void Update(){
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //casts a ray from the center of the screen
         RaycastHit hit;
+
         if (Physics.Raycast(ray, out hit)){ //checks if the ray hit anything and stores information in hit if something was hit
             var selectionTransform = hit.transform; //grabs hit object
 
@@ -25,8 +27,19 @@ public class SelectionManager : MonoBehaviour
                 InteractionInfoUI.SetActive(true);
             }//end of if
             else{
-                InteractionInfoUI.SetActive(false); //shows nothing if object is nnot interactable
+                InteractionInfoUI.SetActive(false); //shows nothing if object is not interactable
             }//end of else
+        }//end of if
+
+        //for chopping down trees
+        if (Input.GetMouseButtonDown(0)){ //use left click to chop (0-left, 1-right, 2-scroller)
+            //chop tree if player is within chopping distance and it is an interactable tree
+            if (hit.collider.CompareTag("Tree") && hit.distance<=choppingDistance){
+                TreeDrops drops = hit.collider.GetComponent<TreeDrops>(); //get the tree component
+                if (drops != null){
+                    drops.Chop();
+                }//end of if
+            }//end of if
         }//end of if
     }//end of Update
 }//end of SelectionManager
